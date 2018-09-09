895c49d2a384a69dd03dfc601e9f647a60b58c8c
nicky <nicky@nickys-MacBook-Pro.local>
2018-09-09 23:27:31 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,73 @@
+<?php
+
+  add_action('admin_menu', 'lorem_ipsum_backend');
+
+  function lorem_ipsum_backend() {
+
+    add_menu_page(
+        'My Top Level Menu Example',
+        'Top Level Menu',
+        'manage_options',
+        'myplugin/myplugin-admin-page.php',
+        'myplguin_admin_page',
+        'dashicons-tickets', 6  );
+
+  }
+
+  function myplguin_admin_page(){
+
+    $sourceUrl = 'https://de.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=1';
+
+    $ch = curl_init($sourceUrl);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt ($ch, CURLOPT_USERAGENT, "Nickyreinert"); // required by wikipedia.org server; use YOUR user agent with YOUR contact information. (otherwise your IP might get blocked)
+    $c = curl_exec($ch);
+
+    $json = json_decode($c);
+var_dump($json);
+    $content = $json->{'parse'}->{'text'}->{'*'}; // get the main text content of the query (it's parsed HTML)
+
+    // pattern for first match of a paragraph
+    $pattern = '#<p>(.*?)</p>#s'; // http://www.phpbuilder.com/board/showthread.php?t=10352690
+    if(preg_match_all($pattern, $content, $matches))
+    {
+        // print $matches[0]; // content of the first paragraph (including wrapping <p> tag)
+        print strip_tags(implode("\n\n",$matches[1])); // Content of the first paragraph without the HTML tags.
+    }
+
+
+    return false;
+
+    // Initialize the post ID to -1. This indicates no action has been taken.
+    $post_id = -1;
+
+    // Setup the author, slug, and title for the post
+    $author_id = 1;
+    $slug = 'example-post';
+    $title = 'My Example Post';
+
+    // If the page doesn't already exist, then create it
+    if( null == get_page_by_title( $title ) ) {
+
+    	// Set the page ID so that we know the page was created successfully
+    	$post_id = wp_insert_post(
+    		array(
+    			'comment_status'	=>	'closed',
+    			'ping_status'		=>	'closed',
+    			'post_author'		=>	$author_id,
+    			'post_name'		=>	$slug,
+    			'post_title'		=>	$title,
+    			'post_status'		=>	'publish',
+    			'post_type'		=>	'post'
+    		)
+    	);
+
+    // Otherwise, we'll stop and set a flag
+    } else {
+
+        // Arbitrarily use -2 to indicate that the page with the title already exists
+        $post_id = -2;
+
+    } // end if
+
+  }
---
+++ loremipsum.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @link              https://www.nickyreinert.de
+ * @since             1.0.0
+ * @package           LoremIpsum
+ *
+ * @wordpress-plugin
+ * Plugin Name:       LoremIpsum
+ * Plugin URI:        https://www.nickyreinert.de
+ * Description:
+ * Version:           1.0.0
+ * Author:            Nicky Reinert
+ * Author URI:        https://www.nickyreinert.de
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       loremipsum
+ * Domain Path:       /languages
+ */
+
+ // If this file is called directly, abort.
+ if ( ! defined( 'WPINC' ) ) {
+ 	die;
+ }
+
+ 	require_once('functions.php');
